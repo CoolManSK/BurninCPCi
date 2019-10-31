891739728fcda5bba8e44e5b6300c6ab90873650
Vladimir Kolman <vladimir.kolman@psbel.com>
2019-10-31 13:48:16 +0100

    Add project files.

---
+++ BurninCPCi/BIShelf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace BurninCPCi
+{
+    class BIShelf
+    {                
+        BIShelf()
+        {
+
+        }
+
+        BIShelf(int Position)
+        {
+            Group
+
+
+        }
+    }
+}
